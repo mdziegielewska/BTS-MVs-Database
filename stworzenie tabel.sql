fa1ead4cf6ea94c48d1d38df2ab3703865f918fa
Marta Dzięgielewska <marta.dziegielewska99@gmail.com>
2020-10-17 13:56:03 +0000

    Update stworzenie tabel.sql

--- stworzenie tabel.sql
+++ stworzenie tabel.sql
@@ -3,16 +3,16 @@ use bts_database;
 
 create table users
 (
-    Ids int not null primary key auto_increment,
+	Ids int not null primary key auto_increment,
     Usernames varchar(10) not null,
     Passwords varchar(30) not null,
-    Display_names varchar(20) not null,
+	Display_names varchar(20) not null,
     Avatar blob
 );
 
 create table bts_members 
 (
-    Stage_name varchar(10) not null,
+	Stage_name varchar(10) not null,
     Stage_name_in_Hangul varchar(30) not null,
     Korean_name varchar(50) not null,
     Korean_name_in_Hangul varchar(50) not null,
@@ -27,12 +27,12 @@ insert into bts_members values ("Suga", "슈가", "Min Yoongi", "민윤기", '19
 insert into bts_members values ("JHope", "제이홉", "Jung Hoseok", "정호석", '1994-02-18', "Gwangju", "Main Dancer, Sub Rapper, Sub Vocalist");
 insert into bts_members values ("Jimin", "지민", "Park Jimin", "박지민", '1995-10-13', "Busan", "Main Dancer, Lead Vocalist");
 insert into bts_members values ("V", "뷔", "Kim Taehyung", "김태형", '1995-12-30', "Daegu", "Lead Dancer, Sub Vocalist, Visual");
-insert into bts_members values ("Jungkook", "정국" "Jeon Jeongguk", "전정국", '1997-09-01', "Busan", "Main Vocalist, Lead Dancer, Sub Rapper, Center, Maknae");
+insert into bts_members values ("Jungkook", "정국", "Jeon Jeongguk", "전정국", '1997-09-01', "Busan", "Main Vocalist, Lead Dancer, Sub Rapper, Center, Maknae");
 select * from bts_members;
 
 create table bts_mvs
 (
-    Song_name varchar(30) not null,
+	Song_name varchar(60) not null,
     Original_name varchar(60),
     Released_date date not null,
     Length time not null,
@@ -54,10 +54,10 @@ insert into bts_mvs values("ON (Kinetic Manifesto Film: Come Prima)", "온", '20
 insert into bts_mvs values("Black Swan (Art Film performed by MN Dance Company)", "흑조", '2020-01-17', '5:31', "Korean", "https://www.youtube.com/watch?v=vGbuUFRdYqU", "Lumpens", "RM", "RM");
 insert into bts_mvs values("Make It Right (feat Lauv)", "메이킷 롸잇", '2019-10-18', '4:18', "Korean", "https://www.youtube.com/watch?v=eXBu09fwe3I", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
 insert into bts_mvs values("Lights", "ライト", '2019-07-02', '5:27', "Japanese", "https://www.youtube.com/watch?v=eaUpme4jalE", "GDW", "-", "-");
-insert into bts_mvs values("Heartbeat", '심장박동', '4:16', "Korean", "https://www.youtube.com/watch?v=aKSxbt-O6TA", "Ahn Jiyoung", "RM", "RM");
+insert into bts_mvs values("Heartbeat", '심장박동', '2019-06-28', '4:16', "Korean", "https://www.youtube.com/watch?v=aKSxbt-O6TA", "Ahn Jiyoung", "RM", "RM");
 insert into bts_mvs values("Boy With Luv (feat Halsey) (ARMY With Luv Ver)", "작은것들을위한시",'2019-04-26', '4:03', "Korean", "https://www.youtube.com/watch?v=62E_xyj_oDA", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
 insert into bts_mvs values("Boy With Luv (feat Halsey)", "작은것들을위한시",'2019-04-12', '4:13', "Korean", "https://www.youtube.com/watch?v=XsX3ATc3FbA", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
-insert into bts_mvs values("Airplane pt2", "飛行機 pt2" '2018-11-08', '3:47', "Japanese", "https://www.youtube.com/watch?v=CxnJf0tWu48", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
+insert into bts_mvs values("Airplane pt2", "飛行機 pt2", '2018-11-08', '3:47', "Japanese", "https://www.youtube.com/watch?v=CxnJf0tWu48", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
 insert into bts_mvs values("Idol (feat Nicki Minaj)", "아이돌", '2018-09-06', '4:54', "Korean", "https://www.youtube.com/watch?v=K1scjjbfNsk", "Lumpens", "RM", "RM");
 insert into bts_mvs values("Idol", "아이돌", '2018-08-24', '3:51', "Korean", "https://www.youtube.com/watch?v=pBuZEGYXA6E", "Lumpens", "RM", "RM");
 insert into bts_mvs values("Fake Love (Extended Ver)", "페이크러브", '2018-06-01', '6:22', "Korean", "https://www.youtube.com/watch?v=D_6QmL6rExk", "Lumpens", "RM", "RM");
@@ -76,13 +76,13 @@ insert into bts_mvs values("Run (Japanese Ver)", "走る", '2016-03-11', '3:55',
 insert into bts_mvs values("Run", "런", '2015-11-29', '7:31', "Korean", "https://www.youtube.com/watch?v=5Wn85Ge22FQ", "Lumpens", "RM, Suga, JHope, V, Jungkook", "RM, Suga, JHope, V, Jungkook");
 insert into bts_mvs values("I NEED U (Japanese Ver)", " 私はあなたが必要", '2015-12-01', '3:39', "Japanese", "https://www.youtube.com/watch?v=LYAcYSmaLoc", "Lumprns", "RM", "RM");
 insert into bts_mvs values("FOR YOU", "あなたのために", '2015-06-05', '5:06', "Japanese", "https://www.youtube.com/watch?v=TTG6nxwdhyA", "Zanybros", "RM, Suga, JHope", "RM, Suga, JHope");
-insert into bts_mvs values("DOPE", "쩔어" '2015-05-23', '4:16', "Korean", "https://www.youtube.com/watch?v=BVwAVbKYYeM", "GDW", "RM, Suga, JHope", "RM, Suga, JHope");
-insert into bts_mvs values("I NEED U", "아이니드유" '2015-04-29', '3:40', "Korean", "https://www.youtube.com/watch?v=NMdTd9e-LEI", "Lumpens", "RM", "RM");
-insert into bts_mvs values("War Of Hormone", "호르몬전쟁" '2014-10-21', '4:58', "Korean", "https://www.youtube.com/watch?v=XQmpVHUi-0A", "Zanybros", "RM, Suga, JHope", "RM, Suga, JHope");
+insert into bts_mvs values("DOPE", "쩔어", '2015-05-23', '4:16', "Korean", "https://www.youtube.com/watch?v=BVwAVbKYYeM", "GDW", "RM, Suga, JHope", "RM, Suga, JHope");
+insert into bts_mvs values("I NEED U", "아이니드유", '2015-04-29', '3:40', "Korean", "https://www.youtube.com/watch?v=NMdTd9e-LEI", "Lumpens", "RM", "RM");
+insert into bts_mvs values("War Of Hormone", "호르몬전쟁", '2014-10-21', '4:58', "Korean", "https://www.youtube.com/watch?v=XQmpVHUi-0A", "Zanybros", "RM, Suga, JHope", "RM, Suga, JHope");
 insert into bts_mvs values("Danger", "댄저", '2014-08-19', '4:59', "Korean", "https://www.youtube.com/watch?v=bagj78IQ3l0", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
 insert into bts_mvs values("Just One Day", "하루만", '2014-04-06', '4:08', "Korean", "https://www.youtube.com/watch?v=DTcKkcyS410", "Lumpens", "RM, Suga, JHope", "RM, Suga, JHope");
-insert into bts_mvs values("Boy In Luv (Dance Ver)", "상남자" '2014-02-20', '3:59', "Korean", "https://www.youtube.com/watch?v=V7RL1V15OTY", "Lumpens", "RM, Suga", "RM, Suga");
-insert into bts_mvs values("Boy In Luv", "상남자" '2014-02-11', '4:42', "Korean", "https://www.youtube.com/watch?v=m8MfJg68oCs", "Lumpens", "RM, Suga", "RM, Suga");
+insert into bts_mvs values("Boy In Luv (Dance Ver)", "상남자", '2014-02-20', '3:59', "Korean", "https://www.youtube.com/watch?v=V7RL1V15OTY", "Lumpens", "RM, Suga", "RM, Suga");
+insert into bts_mvs values("Boy In Luv", "상남자", '2014-02-11', '4:42', "Korean", "https://www.youtube.com/watch?v=m8MfJg68oCs", "Lumpens", "RM, Suga", "RM, Suga");
 insert into bts_mvs values("We Are Bulletproof pt2", "위아불렛프루프 Pt.2", '2013-07-16', '3:52', "Korean", "https://www.youtube.com/watch?v=gqhWHy0rrtM", "Zanybros", "RM, Suga", "RM, Suga");
 insert into bts_mvs values("N.O", "엔.오", '2013-07-10', '4:02', "Korean", "https://www.youtube.com/watch?v=mmgxPLLLyVo", "Zanybros", "RM, Suga", "RM, Suga");
 insert into bts_mvs values("No More Dream (Dance Ver)", "노모어드림", '2013-06-17', '4:00', "Korean", "https://www.youtube.com/watch?v=pUkViXyQTI4", "Zanybros", "RM, Suga, JHope", "RM, Suga, JHope");
